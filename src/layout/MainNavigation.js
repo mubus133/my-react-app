115b8a876e9faf0388ebc289973c4cffa55db452
mubus133 <usmanmubarak5652@dmail.com>
2024-12-14 13:48:35 +0100

    rr

--- src/layout/MainNavigation.js
+++ src/layout/MainNavigation.js
@@ -9,7 +9,7 @@ function MainNavigation() {
     const favoritesCtx = useContext(FavoritesContext);
     return (
         <header className={classes.header}>
-            <div className={classes.logo}>React Meetups</div>
+            <div className={classes.logo}>Meetups</div>
             <nav>
                 <ul>
                     <li>
